9259a1b58585c86afbc6995034d80675fe14f969
Mark Alfano <mpalfano4@gmail.com>
2023-02-26 19:54:20 -0500

    small change

--- src/components/Ticket.tsx
+++ src/components/Ticket.tsx
@@ -2,9 +2,7 @@ import React from "react";
 
 const Ticket = (props: any) => {
   return (
-    <p className="ticket mt-3 p-4 bg-blue-200 rounded-lg click:cursor-grabbing">
-      {props.children}
-    </p>
+    <p className="ticket mt-3 p-4 bg-blue-200 rounded-lg">{props.children}</p>
   );
 };
 
